18be9e6e9418cd7332b51b0e5032b3f08b54949a
ftmu <ftmu@technodom.kz>
2024-05-16 15:00:35 +0500

    improving styles

--- src/components/Calendar/DoctorTitle/DoctorTitle.tsx
+++ src/components/Calendar/DoctorTitle/DoctorTitle.tsx
@@ -1,27 +1,41 @@
 import styles from './styles.module.scss'
-import {Avatar, Typography} from "antd";
-import { UserOutlined, CalendarOutlined } from '@ant-design/icons';
+import {Avatar, Tooltip, Typography} from "antd";
+import {UserOutlined} from '@ant-design/icons';
+import calendarIcon from '../../../assets/calendarIcon.svg'
 
-const { Title } = Typography;
+const {Title} = Typography;
 
 type Props = {
-  doctor: string,
+    doctor: string,
     count?: number
     isEmpty?: boolean,
 }
 const DoctorTitle = ({doctor, isEmpty = false, count}: Props) => {
     return (
         <div className={styles.container}>
-            <div className={styles.container_content}>
-                <div className={styles.container_content_count}>
-                    {count}
-                </div>
-                <div className={styles.container_content_info}>
-                    {!isEmpty && <Avatar size="small" icon={<UserOutlined/>}/>}
-                    <Title style={{fontSize: 14}}>{doctor}</Title>
-                    {!isEmpty && <CalendarOutlined/>}
-                </div>
+            <div className={styles.container_count}>{count}</div>
+            <div className={styles.container_info}>
+                {!isEmpty && <Avatar size="small" icon={<UserOutlined/>}/>}
+                <Tooltip title={doctor}>
+                    <Title
+                        style={{
+                            fontSize: 14,
+                            whiteSpace: 'nowrap',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis'
+                        }}>{doctor}</Title>
+                </Tooltip>
             </div>
+            <div className={styles.container_calendar}>
+                {!isEmpty && <img src={calendarIcon}
+                                  width={20}
+                                  height={20}
+                    style={{
+                        fontSize: '17px',
+                        fontWeight: 800
+                    }}/>}
+            </div>
+
         </div>
     );
 };
