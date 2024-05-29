205b5fbed5e217ab82bdd772dda4eeec1e49b39f
Tolegen777 <tolegenmukan@gmail.com>
2024-05-29 20:08:00 +0500

    fix modal styles

--- src/components/Calendar/Slot/SlotEditBlockContent/index.tsx
+++ src/components/Calendar/Slot/SlotEditBlockContent/index.tsx
@@ -1,22 +1,23 @@
-import {memo, useEffect, useMemo, useRef, useState} from "react";
-import {Box, boxesIntersect, useSelectionContainer} from "@air/react-drag-to-select";
+import React, {memo, useMemo, useRef, useState} from "react";
 import styles from "../SlotEditBlock/styles.module.scss";
-import {TimeSlotContent} from "../TimeSlotContent";
 import {WorkingHoursList} from "../../../../types/calendar.ts";
+import {ReactMouseSelect, TFinishSelectionCallback} from "react-mouse-select";
+import {TimeSlotContent} from "../TimeSlotContent";
 
 type Props = {
     workingHours: WorkingHoursList[]
 }
 
 export const SlotEditBlockContent = memo(({ workingHours }: Props) => {
-    const [selectedTimeSlotIds, setSelectedTimeSlotIds] = useState<Set<number>>(new Set());
-    const selectableItems = useRef<Box[]>([]);
-    const elementsContainerRef = useRef<HTMLDivElement | null>(null);
+    const [selectedTimeSlotIds, setSelectedTimeSlotIds] = useState<number[]>([]);
+
+    const borderSelectionContainer = document.getElementById('root') as HTMLElement;
+    const containerRef = useRef<HTMLElement>(null);
 
     const formatWorkingHours = () => {
         return workingHours.map((item) => ({
             ...item,
-            isActive: selectedTimeSlotIds.has(item.time_slot_id)
+            isActive: selectedTimeSlotIds.includes(item.time_slot_id)
         }));
     };
 
@@ -25,88 +26,41 @@ export const SlotEditBlockContent = memo(({ workingHours }: Props) => {
         [selectedTimeSlotIds]
     );
 
-    const { DragSelection } = useSelectionContainer({
-        // @ts-ignore
-        eventsElement: document,
-        onSelectionChange: (box) => {
-            const scrollAwareBox: Box = {
-                ...box,
-                top: box.top + window.scrollY,
-                left: box.left + window.scrollX
-            };
-            const indexesToSelect: number[] = [];
-            selectableItems.current.forEach((item, index) => {
-                if (boxesIntersect(scrollAwareBox, item)) {
-                    const selectedId = memoizedTimeSlots[index]?.time_slot_id;
-                    const isAvailable = memoizedTimeSlots[index]?.doctor_availability;
-                    if (isAvailable) {
-                        indexesToSelect.push(selectedId);
-                    }
-                }
-            });
-
-            setSelectedTimeSlotIds(prevState => {
-                const newState = new Set(prevState);
-                indexesToSelect.forEach(id => newState.add(id));
-                return newState;
-            });
-        },
-        onSelectionStart: () => {
-            console.log("OnSelectionStart");
-        },
-        onSelectionEnd: () => console.log("OnSelectionEnd"),
-        selectionProps: {
-            style: {
-                border: "2px dashed purple",
-                borderRadius: 4,
-                backgroundColor: "brown",
-                opacity: 0.5
-            }
-        },
-        isEnabled: true
-    });
-
-    useEffect(() => {
-        if (elementsContainerRef.current) {
-            Array.from(elementsContainerRef.current.children).forEach((item) => {
-                const { left, top, width, height } = item.getBoundingClientRect();
-                selectableItems.current.push({
-                    left,
-                    top,
-                    width,
-                    height
-                });
-            });
+    const finishSelection: TFinishSelectionCallback = (items) => {
+        const selectedIds = items
+            ?.map(item => parseInt(item.getAttribute('data-id') || ''))
+            ?.filter(item => !isNaN(item));
+        console.log(selectedIds, 'IDS')
+        if (selectedIds.length > 0) {
+            setSelectedTimeSlotIds(selectedIds);
         }
-    }, []);
-
-    useEffect(() => {
-        if (workingHours.length) {
-            const reservedItems = workingHours.filter(item => item.reserved).map(item => item.time_slot_id);
-            setSelectedTimeSlotIds(prevState => {
-                const newState = new Set(prevState);
-                reservedItems.forEach(id => newState.add(id));
-                return newState;
-            });
-        }
-    }, [workingHours]);
+    }
 
     return (
         <div>
-            <DragSelection />
-            <div
+            <main
                 className={styles.container_content}
-                ref={elementsContainerRef}
+                ref={containerRef}
+                style={{padding: '40px'}}
             >
                 {memoizedTimeSlots?.map(item =>
-                    <TimeSlotContent
-                        key={item?.time_slot_id}
-                        timeSlot={item}
-                        setSelectedTimeSlotIds={setSelectedTimeSlotIds}
-                        selectedTimeSlotIds={selectedTimeSlotIds}
-                    />
+                        <TimeSlotContent
+                            key={item?.time_slot_id}
+                            timeSlot={item}
+                            setSelectedTimeSlotIds={setSelectedTimeSlotIds}
+                            selectedTimeSlotIds={selectedTimeSlotIds}
+                        />
                 )}
-            </div>
+            </main>
+            <ReactMouseSelect
+                containerRef={containerRef}
+                portalContainer={borderSelectionContainer}
+                itemClassName="mouse-select__selectable"
+                sensitivity={10}
+                tolerance={5}
+                notStartWithSelectableElements={false}
+                finishSelectionCallback={finishSelection}
+            />
         </div>
     );
 });
--- src/components/Calendar/Slot/TimeSlotContent/index.tsx
+++ src/components/Calendar/Slot/TimeSlotContent/index.tsx
@@ -6,18 +6,19 @@ import {Dispatch, memo, SetStateAction} from "react";
 
 type Props = {
     timeSlot: WorkingHoursList;
-    setSelectedTimeSlotIds: Dispatch<SetStateAction<Set<number>>>;
-    selectedTimeSlotIds: Set<number>;
+    setSelectedTimeSlotIds: Dispatch<SetStateAction<number[]>>;
+    selectedTimeSlotIds: number[];
 }
 
 export const TimeSlotContent = memo(({timeSlot, setSelectedTimeSlotIds, selectedTimeSlotIds}: Props) => {
     const handleChangeTimeSlot = (item: WorkingHoursList) => {
         setSelectedTimeSlotIds(prevState => {
-            const newState = new Set(prevState);
-            if (newState.has(item.time_slot_id)) {
-                newState.delete(item.time_slot_id);
+            const newState = [...prevState];
+            const index = newState.indexOf(item.time_slot_id);
+            if (index !== -1) {
+                newState.splice(index, 1);
             } else if (item.doctor_availability) {
-                newState.add(item.time_slot_id);
+                newState.push(item.time_slot_id);
             }
             return newState;
         });
@@ -28,9 +29,11 @@ export const TimeSlotContent = memo(({timeSlot, setSelectedTimeSlotIds, selected
             className={clsx({
                 [styles.container]: true,
                 [styles.container_not_available]: !timeSlot?.doctor_availability,
-                [styles.container_reserved]: selectedTimeSlotIds.has(timeSlot?.time_slot_id),
+                [styles.container_reserved]: selectedTimeSlotIds.includes(timeSlot?.time_slot_id),
+                ['mouse-select__selectable']: true
             })}
             onClick={() => handleChangeTimeSlot(timeSlot)}
+            data-id={timeSlot?.doctor_availability ? timeSlot?.time_slot_id : null}
         >
             {removeSeconds(timeSlot?.start_time)}
         </div>
